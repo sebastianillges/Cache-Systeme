72737fb4f9f5ebbf5ae8ea48f58947d2b9fb7960
Sebastian Illges <sebastian.illges@gmail.com>
2024-01-06 12:11:43 +0100

    add 80-20 function

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,31 @@ struct DatenQuelleCache {
 
 
 fn main() {
-    let mut cache = DatenQuelleCache::new(1000, Duration::from_secs(30));
     let file_path = "telefonbuch.json";
 
-    test_without_cache(1000, file_path);
-    test_cache(cache, 1000, file_path);
+    test_cache_random(500, 1000, file_path);
+    test_cache_80_20(500, 1000, file_path);
+    test_without_cache_random(1000, file_path);
 }
 
-fn test_without_cache(num_keys: i32, file_path: &str) {
+fn test_cache_80_20(cache_size: usize, num_keys: i32, file_path: &str) {
+    let start = Instant::now();
+    let mut cache = DatenQuelleCache::new(cache_size, Duration::from_secs(30));
+    for _i in 0..10000 {
+        let rand_num = rand::thread_rng().gen_range(1..10);
+        let mut rand_area = 0;
+        if rand_num < 3 {
+            rand_area = rand::thread_rng().gen_range(1..(num_keys as f32 * 0.8) as i32);
+        } else {
+            rand_area = rand::thread_rng().gen_range((num_keys as f32 * 0.8) as i32..num_keys);
+        }
+        let _ = cache.abrufen(&rand_num.to_string(), file_path);
+    }
+    let total_time = start.elapsed();
+    println!("Total time: {:?}", total_time);
+}
+
+fn test_without_cache_random(num_keys: i32, file_path: &str) {
     let start = Instant::now();
     for _i in 0..10000 {
         let rand_num = rand::thread_rng().gen_range(1..num_keys).to_string();
@@ -34,9 +51,10 @@ fn test_without_cache(num_keys: i32, file_path: &str) {
     println!("Total time: {:?}", total_time);
 }
 
-fn test_cache(mut cache: DatenQuelleCache, num_keys: i32, file_path: &str) {
+fn test_cache_random(cache_size: usize, num_keys: i32, file_path: &str) {
+    let mut cache = DatenQuelleCache::new(cache_size, Duration::from_secs(30));
     for _i in 0..10000 {
-        let rand_num = rand::thread_rng().gen_range(1..1000).to_string();
+        let rand_num = rand::thread_rng().gen_range(1..num_keys).to_string();
         let _ = cache.abrufen(&rand_num, file_path);
     }
     println!("Hits: {}", cache.hits);
